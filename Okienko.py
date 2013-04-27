93b0337f0ed1b81cd180ab06d4ab5f51b27620ca
d33tah <d33tah@localhost.localdomain>
2013-04-27 19:02:27 +0200

    działa przesuwanie wojsk dodano widok historii gry przyciski do przelewania punktów i kupowania jednostek

--- Okienko.py
+++ Okienko.py
@@ -31,22 +31,71 @@ class Okienko(QtGui.QMainWindow):
         self.ui.nrTuryLbl.connect(self.timer, QtCore.SIGNAL('timeout()'), self.odswiez_ekran)
         self.timer.start(1000)
         
+        self.tryb_przyciskow = None
+        self.ruch_z = None
+        
         self.ui.podlaczSieBtn.clicked.connect(lambda: self.podlacz_sie())
         self.ui.nowaTuraBtn.clicked.connect(lambda: Protokol.nowa_tura())
+        
+        pionowo = QtGui.QVBoxLayout()
+        self.rozmiar_planszy = 5
+        self.ui.widget.setLayout(pionowo)
+        self.przyciski = [ [None for _ in range(self.rozmiar_planszy)] for _ in range(self.rozmiar_planszy) ]
+        for y in range(self.rozmiar_planszy):
+            poziomo = QtGui.QHBoxLayout()
+            for x in range(self.rozmiar_planszy):
+                wdg = QtGui.QPushButton()
+                wdg.clicked.connect(lambda wdg=wdg, x=x, y=y: self.wcisnieto_przycisk(x, y))
+                poziomo.addWidget(wdg)
+                self.przyciski[x][y] = wdg
+            pionowo.addLayout(poziomo)
+            
+    def wcisnieto_przycisk(self, x, y):
+        
+        if self.tryb_przyciskow is None:
+            if Protokol.plansza[x][y] == '!':
+                self.ruch_z = (x, y)
+                self.tryb_przyciskow = "PRZESUN"
+            else:
+                QtGui.QMessageBox.critical(self, u'Błąd', "Tu nie ma twoich wojsk!", QtGui.QMessageBox.Ok, QtGui.QMessageBox.Ok)
+        elif self.tryb_przyciskow == "PRZESUN":
+            stary_x, stary_y = self.ruch_z
+            if y == stary_y and (x == stary_x + 1 or x == stary_x - 1) or \
+                    x == stary_x and (y == stary_y + 1 or y == stary_y - 1):
+                Protokol.wyslij_przesun(self.ruch_z, (x, y))
+                Protokol.plansza[stary_x][stary_y] = None
+                Protokol.plansza[x][y] = "!"
+                self.tryb_przyciskow = None
+            else:
+                QtGui.QMessageBox.critical(self, u'Błąd', "Niedozwolony ruch: %s,%s" % (x, y), QtGui.QMessageBox.Ok, QtGui.QMessageBox.Ok)
+            
+
     
     def podlacz_sie(self):
         try:
-            Protokol.podlacz_sie("0.0.0.0", int(self.ui.numerPortuEdit.text()))
+            Protokol.podlacz_sie(self.ui.adresIpEdit.text(), int(self.ui.numerPortuEdit.text()))
         except socket.error, e:
             QtGui.QMessageBox.critical(self, u'Błąd', unicode(e), QtGui.QMessageBox.Ok, QtGui.QMessageBox.Ok)
             self.ui.statusbar.showMessage(str(e))
     
     def odswiez_ekran(self):
+        
+        self.ui.historiaGryEdit.setText(Protokol.historia_gry)
+        cur = self.ui.historiaGryEdit.textCursor()
+        cur.movePosition(QtGui.QTextCursor.End)
+        self.ui.historiaGryEdit.setTextCursor(cur)
+        for x in range(self.rozmiar_planszy):
+            for y in range(self.rozmiar_planszy):
+                pkt = Protokol.plansza[x][y]
+                self.przyciski[x][y].setText('' if pkt is None else pkt) 
+        
         if Protokol.gniazdo:
+            self.ui.panelGry.setEnabled(not Protokol.zglosilem)
             self.ui.nrTuryLbl.setText("Numer tury: %s" % Protokol.nr_tury)
         if Protokol.port_nasluchu:
             self.statusBar().showMessage(u"Nasłuchuję na porcie %s" % Protokol.port_nasluchu)
             
         self.ui.peerList.clear()
         for gniazdo in Protokol.podlaczeni:
-            self.ui.peerList.addItem("(K) %s:%s" % gniazdo.client_address)
+            self.ui.peerList.addItem("(%s) %s:%s" % gniazdo)
+            
--- Protokol.py
+++ Protokol.py
@@ -1,13 +1,13 @@
+# -*- coding: utf-8 -*-
+
 import logging
 import SocketServer
 import threading
 import socket
-from PyQt4 import QtCore
-
 
 class Protokol(SocketServer.BaseRequestHandler):
     
-    plansza = [ [None for _ in range(10)] for _ in range(10) ]
+    plansza = [ [None for _ in range(5)] for _ in range(5) ]
     nr_tury = 0
     zglosilem = False
     zglosil = False
@@ -15,33 +15,68 @@ class Protokol(SocketServer.BaseRequestHandler):
     okno = None
     port_nasluchu = None
     podlaczeni = []
+    historia_gry = ""
     
     @classmethod
-    def odczytuj(self):
+    def rozpocznij_gre(self, czy_serwer):
+        if czy_serwer:
+            self.plansza[0][0] = "X"
+            self.plansza[4][4] = "!"
+        else:
+            self.plansza[0][0] = "!"
+            self.plansza[4][4] = "X"
+            
+    @classmethod
+    def koniec_tury(self):
+        self.dopisz(u"Przyjęto koniec tury.")
+        logging.debug("KONIECTURY")
+        self.zglosil = True
+        if self.zglosilem:
+            self.nr_tury += 1
+            self.zglosilem = False
+            self.zglosil = False
+            logging.debug("Nowa tura, nr=%s" % self.nr_tury)
+    
+    @classmethod
+    def przesun(self, komenda):
+        s_x, s_y, x, y = map(int, komenda[1:])
+        self.dopisz(u"Przeciwnik sie przesunął: %s,%s->%s,%s" % (s_x, s_y, x, y))
+        self.dopisz(u"Koniec tury.")
+        self.plansza[s_x][s_y] = None
+        self.plansza[x][y] = "X"
+    
+    @classmethod
+    def odczytuj(self, czy_serwer = True):
+        self.rozpocznij_gre(czy_serwer)
         if self.gniazdo is None:
             self.gniazdo = self.request
         logging.info("Siedze tu.")
         while True:
             data = self.gniazdo.recv(1024).rstrip()
-            logging.debug("k_data=%s" % data)
+            #logging.debug("k_data=%s" % data)
             komenda = data.split(' ')
             if komenda[0]=='KONIECTURY':
-                logging.debug("KONIECTURY")
-                self.zglosil = True
-                if self.zglosilem:
-                    self.nr_tury += 1
-                    self.zglosilem = False
-                    self.zglosil = False
-                    logging.debug("Nowa tura, nr=%s" % self.nr_tury)
+                self.koniec_tury()
+            if komenda[0]=="PRZESUN":
+                self.przesun(komenda)
 
+    @classmethod
+    def wyslij_przesun(self, stary_pos, nowy_pos):
+        s_x, s_y = stary_pos 
+        x, y = nowy_pos
+        self.gniazdo.send("PRZESUN %s %s %s %s" % ( s_x, s_y, x, y))
+        self.dopisz(u"Przesunięto się: %s,%s->%s,%s" % (s_x, s_y, x, y))
     
     def handle(self):
         Protokol.gniazdo = self.request
-        Protokol.podlaczeni += [self]
-        Protokol.odczytuj()
+        host, port = self.client_address
+        Protokol.podlaczeni += [("K", host, port)]
+        self.dopisz(u"Przyjęto połączenie od %s:%s" % (host, port))
+        Protokol.odczytuj(False)
      
     @classmethod               
     def nowa_tura(self):
+        self.dopisz(u"Ogłoszono koniec tury.")
         logging.debug("Wysylam")
         self.zglosilem = True
         self.gniazdo.send("KONIECTURY")
@@ -55,7 +90,13 @@ class Protokol(SocketServer.BaseRequestHandler):
     def podlacz_sie(self, host, port):
         Protokol.gniazdo = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         Protokol.gniazdo.connect((host, port))
+        self.podlaczeni += [("S", host, port)]
+        self.dopisz(u"Podłączono do %s:%s" % (host, port))
         threading.Thread(target=Protokol.odczytuj).start()
+        
+    @classmethod
+    def dopisz(self, tekst):
+        self.historia_gry += "%s\n" % tekst
         
     @classmethod
     def uruchom_serwer(self):
